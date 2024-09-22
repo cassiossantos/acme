3af4d80753825f528a5364b8f3170a66e44de860
Cássio Santos <cassio.slv1654@gmail.com>
2024-09-22 20:15:19 -0300

    chore: updating dashboard layouts and creating components

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -4,9 +4,20 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { CashFlowPresenterComponent } from './presenters/cash-flow.component';
 import { CashFlowContainerComponent } from './containers/cash-flow.container';
 import { MaterialModule } from '../../shared/material.module';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { TableComponent } from './components/table/table.component';
+import { CardComponent } from './components/card/card.component';
+import { PaginatorComponent } from './components/paginator/paginator.component';
 
 @NgModule({
-  declarations: [CashFlowPresenterComponent, CashFlowContainerComponent],
+  declarations: [
+    CashFlowPresenterComponent,
+    CashFlowContainerComponent,
+    TopbarComponent,
+    TableComponent,
+    CardComponent,
+    PaginatorComponent,
+  ],
   imports: [CommonModule, DashboardRoutingModule, MaterialModule],
 })
 export class DashboardModule {}
